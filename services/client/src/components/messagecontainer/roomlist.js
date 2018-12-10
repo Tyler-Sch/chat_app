23b34ccd9b7fadf8f065c2fe418e130b952d908b
Tyler <tylerschauer@gmail.com>
2018-12-10 10:22:54 -0500

    a few small changes while trying to figure out the css

--- services/client/src/components/messagecontainer/roomlist.js
+++ services/client/src/components/messagecontainer/roomlist.js
@@ -12,7 +12,7 @@ class RoomList extends React.Component {
       return <RoomTile roomInfo={i} />
     });
     return (
-      <div class="is-marginless" id="roomlist">
+      <div class="" id="roomlist">
         <div id="room-list-head">
            <p class="has-text-white-ter subtile is-size-6 has-text-centered ">Rooms</p>
         </div>
@@ -33,7 +33,7 @@ class RoomTile extends React.Component {
     return (
       <div class="RoomTile">
         {this.props.roomInfo.has_checked == true ? (
-          <p class="has-text-grey-light is-size-5">{this.props.roomInfo.room}</p>
+          <p class="has-text-grey-light is-size-5 has-text-left">{this.props.roomInfo.room}</p>
         ):(
           <p class="has-text-danger is-size-5">{this.props.roomInfo.room}</p>
         )}
